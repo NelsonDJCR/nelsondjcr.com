2f51de19a5055dde174d9946629b3b5d0270e2b1
NelsonDJCR <dev@nelsondjcr.com>
2025-07-26 14:55:44 +0200

    update

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,41 +1,36 @@
 export const projectsData = [
     {
-        id: 1,
         role: 'Full Stack Developer',
         company: "AliceHub",
-        description: "At AliceHub, a startup project, I led the full UI/UX process, designed all mockups in Figma, crafted the user experience from scratch, developed the landing page, and built the first sections of the beta app to deliver a solid MVP ready for testing and feedback.",
+        description: "Led UI construction from Figma, built coremodules with a strong focus on user experience,and created a pilot app using Ionic with Svelte totest features and validate the product vision.",
         duration: "(Nov 2023 - Aug 2024)",
-        tools: ['Svelte', 'Figma', 'Laravel', 'ReactJS', 'JavaScript', 'Tailwind', 'CSS', 'MySQL', 'Git','Supabase', 'TypeScript', 'Digital Ocean'],
+        tools: ['Svelte', 'Figma', 'Laravel', 'ReactJS', 'JavaScript', 'Tailwind', 'CSS', 'MySQL', 'Git', 'Supabase', 'TypeScript', 'Digital Ocean'],
     },
     {
-        id: 2,
         role: "PHP Developer",
         company: "Solistica",
-        description: "At Solistica, I worked in the logistics and transport sector, solving bugs in internal apps, improving performance, and optimizing development flow. I focused on boosting operational efficiency through smart code and fast incident resolution across all platforms.",
+        description: "Supported and developed internal webapplications using Laravel for teams in Colombiaand Brazil, ensuring smooth operation and fastissue resolution.",
         duration: "(Mar 2023 - Jun 2024)",
         tools: ['Laravel', 'Docker', 'PHP', 'ReactJS', 'JavaScript', 'Svelte', 'MySQL', 'Git'],
     },
     {
-        id: 3,
         role: "Development Analyst",
         company: "PAR Servicios Integrales",
-        description: "At PAR Servicios, I maintained and enhanced internal web systems, delivered fast solutions for new requirements, and created data export tools to improve operations. I used PHP, Laravel, MongoDB, Git, MySQL, Bootstrap, and worked closely with teams via MS Teams.",
+        description: "Provided support for internal applications usingPHP, Laravel, and maintained modules in Pythonwith MongoDB and MySQL, improving stabilityand data handling in critical systems.",
         duration: "(Feb 2022 - Mar 2023)",
         tools: ['Laravel', 'jQuery', 'MySQL', 'MongoDB', 'PHP', 'JavaScript', 'Bootstrap', 'CSS', 'Git'],
     },
     {
-        id: 4,
         role: "Backend & Mobile Developer",
         company: "Hard Logistic",
-        description: "At Hard Logistic, I rebuilt a platform from scratch with Laravel and AWS, improving scalability and sales. I deployed it on Ubuntu with EC2 and RDS, included PWA, geolocation, and UI/UX upgrades that boosted service connections by 20% across multiple products.",
+        description: "Developed a custom eCommerce platform andmultiple internal tools. Acted as tech lead,owning the entire process and making all keydecisions for development and delivery.",
         duration: "(Oct 2021 - Apr 2022)",
         tools: ['PHP', 'Laravel', 'JavaScript', 'JQuery', 'MySQL', 'AWS', 'Bootstrap', 'CSS', 'PWA', 'Git', 'Ubuntu', 'Digital Ocean', 'AWS'],
     },
     {
-        id: 5,
         role: "FullStack Developer",
         company: "NyG Soft",
-        description: "At NyG Soft, I supported and improved internal apps, built new modules, and developed custom solutions using PHP, Laravel, Vue.js, MySQL, and Bootstrap. I handled versioning with Git and delivered features with strong focus on performance and maintainability.",
+        description: "Supported and improved apps, developedmodules, and built full apps from scratch withPHP, Laravel, Vue.js, jQuery, JavaScript,Bootstrap, MySQL, Git, plus Angular and Ionicfor mobile solutions.",
         duration: "(Feb 2021 - Jul 2021)",
         tools: ['PHP', 'Laravel', 'Jquery', 'Vue', 'CodeIgniter', 'JavaScript', 'Bootstrap', 'CSS', 'MySQL', 'Git'],
     },
@@ -46,7 +41,6 @@ export const projectsData = [
 // Leave it blank instead as shown below
 
 // {
-//     id: 1,
 //     name: '',
 //     description: "",
 //     tools: [],
