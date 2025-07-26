b5ea4a96330dc53532e5a2cf2c9512ab9b8a97a2
NelsonDJCR <dev@nelsondjcr.com>
2025-07-26 11:09:32 +0200

    works

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,18 @@
-"use client"
+"use client";
 
 import Lottie from "lottie-react";
 
 const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationPath,
+        style: {
+            width: width || "95%",
+        },
+    };
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+    return <Lottie {...defaultOptions} />;
 };
 
 export default AnimationLottie;
