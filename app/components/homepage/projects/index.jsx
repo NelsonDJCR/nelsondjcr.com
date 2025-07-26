2f4ff9228eb3a29f2b35a560e8cbe42e2f1ab9fc
NelsonDJCR <dev@nelsondjcr.com>
2025-07-26 11:13:58 +0200

    finish

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,14 +1,21 @@
+"use client";
+
 import { projectsData } from "@/utils/data/projects-data";
 import ProjectCard from "./project-card";
-import AnimationLottie from "../../helper/animation-lottie";
 import experience from "../../../assets/lottie/code.json";
+import dynamic from "next/dynamic";
+
+const AnimationLottie = dynamic(() => import("../../helper/animation-lottie"), {
+    ssr: false,
+});
+
 const Projects = () => {
     return (
-        <div id="projects" className="relative z-50  my-12 lg:my-24">
+        <div id="projects" className="relative z-50 my-12 lg:my-24">
             <div className="sticky top-10">
-                <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
+                <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl opacity-30"></div>
                 <div className="flex items-center justify-start relative">
-                    <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">Experience</span>
+                    <span className="bg-[#1a1443] absolute left-0 w-fit text-white px-5 py-3 text-xl rounded-md">Experience</span>
                     <span className="w-full h-[2px] bg-[#1a1443]"></span>
                 </div>
             </div>
@@ -17,9 +24,10 @@ const Projects = () => {
                 <div className="flex flex-col gap-6">
                     <div className="flex col-span-2 justify-center items-start">
                         <div className="w-[30%] h-[30%]">
-                            {/* <AnimationLottie animationPath={experience} /> */}
+                            <AnimationLottie animationPath={experience} />
                         </div>
                     </div>
+
                     {projectsData.slice(0, 5).map((project, index) => (
                         <div id={`sticky-card-${index + 1}`} key={index} className="sticky-card w-full mx-auto max-w-2xl sticky">
                             <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
